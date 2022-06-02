d30a71e0a0505a7612fc9c11c9cf95683f8c9124
brayanelcrack <bscanon2@misena.edu.co>
2022-06-01 21:13:29 -0500

    01/06/2022

---
+++ Export/modulosJS2/export.js
@@ -0,0 +1,9 @@
+const nombre = "brayanCrack";
+function suma(n1, n2) {
+    return n1+n2;
+}
+module.exports.nom=nombre;
+module.exports={
+    nom:nombre,
+    suma:suma
+}
---
+++ Export/modulosJS2/require.js
@@ -0,0 +1,2 @@
+const operacion = require("./export")
+console.log(operacion.suma(5,5));
